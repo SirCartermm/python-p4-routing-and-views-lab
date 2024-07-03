123c7ec48da6560ba85073e00813aebf7d8e9127
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-03 10:30:05 +0300

    modified app.y

--- server/app.py
+++ server/app.py
@@ -14,3 +14,12 @@ def print_string(param):
 @app.route("/count/<int:param>")
 def count(param):
     return "<br>".join(str(i) for i in range(param))
+
+@app.route("/math/<int:num1>/<string:operation>/<int:num2>")
+def math(num1, operation, num2):
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+       
